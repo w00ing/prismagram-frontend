9cd228d28282cdaf40cd7797cb9884ed6a8ce869
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-31 20:27:48 +0900

    Create Post component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
+import { HashRouter as Router } from "react-router-dom";
 import { useQuery } from "react-apollo-hooks";
 import { ToastContainer, toast } from "react-toastify";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
-import AppRouter from "./Router";
+import Routes from "./Routes";
 
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./Footer";
+import Header from "./Header";
 
 const QUERY = gql`
   {
@@ -18,7 +20,7 @@ const QUERY = gql`
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: 935px;
+  max-width: ${(props) => props.theme.maxWidth};
   width: 100%;
 `;
 
@@ -29,12 +31,17 @@ export default () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <Wrapper>
-        <GlobalStyles />
-        <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
-        <Footer />
-        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
-      </Wrapper>
+      <GlobalStyles />
+      <Router>
+        <>
+          {isLoggedIn && <Header />}
+          <Wrapper>
+            <Routes isLoggedIn={isLoggedIn}></Routes>
+            <Footer />
+            <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
+          </Wrapper>
+        </>
+      </Router>
     </ThemeProvider>
   );
 };
---
+++ src/Components/Post/PostPresenter.js
@@ -0,0 +1,131 @@
+import React from "react";
+import styled from "styled-components";
+import FatText from "../FatText";
+import Avatar from "../Avatar";
+import { HeartFull, HeartEmpty, Comment } from "../Icons";
+import TextareaAutosize from "react-autosize-textarea";
+
+const Post = styled.div`
+  ${(props) => props.theme.whiteBox};
+  width: 100%;
+  max-width: 600px;
+  margin-bottom: 25px;
+`;
+
+const Header = styled.header`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+`;
+
+const UserColumn = styled.div`
+  margin-left: 5px;
+`;
+
+const Location = styled.span`
+  display: block;
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+const Files = styled.div`
+  position: relative;
+  padding-bottom: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  flex-shrink: 0;
+`;
+
+const File = styled.img`
+  max-width: 100%;
+  position: absolute;
+  top: 0;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  background-position: center;
+  opacity: ${(props) => (props.showing ? 1 : 0)};
+  transition: opacity 0.5s linear;
+`;
+
+const Button = styled.span`
+  cursor: pointer;
+`;
+
+const Meta = styled.div`
+  padding: 15px;
+`;
+
+const Buttons = styled.div`
+  ${Button} {
+    &:first-child {
+      margin-right: 10px;
+    }
+  }
+  margin-bottom: 10px;
+`;
+
+const Timestamp = styled.span`
+  font-weight: 400;
+  text-transform: uppercase;
+  opacity: 0.5;
+  display: block;
+  font-size: 12px;
+  margin: 10px 0px;
+  padding-bottom: 10px;
+  border-bottom: ${(props) => props.theme.lightGreyColor} solid 1px;
+`;
+
+const Textarea = styled(TextareaAutosize)`
+  border: none;
+  resize: none;
+  width: 100%;
+  font-size: 14px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default ({
+  user: { username, avatar },
+  location,
+  files,
+  isLiked,
+  likeCount,
+  createdAt,
+  newComment,
+  currentItem,
+}) => (
+  <Post>
+    <Header>
+      <Avatar size="sm" url={avatar} />
+      <UserColumn>
+        <FatText text={username} />
+        <Location>{location}</Location>
+      </UserColumn>
+    </Header>
+    <Files>
+      {files?.map((file, index) => (
+        <File
+          id={file.id}
+          key={file.id}
+          src={file.url}
+          showing={index === currentItem}
+        />
+      ))}
+    </Files>
+    <Meta>
+      <Buttons>
+        <Button>{isLiked ? <HeartFull /> : <HeartEmpty />}</Button>
+        <Button>
+          <Comment />
+        </Button>
+      </Buttons>
+      <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
+      <Timestamp>{createdAt}</Timestamp>
+      <Textarea placeholder={"Add a comment"} {...newComment} />
+    </Meta>
+  </Post>
+);
